e7d333d0a738b2b655e8ca5ccf29bba0a9a7f7c4
Artyom Hayrapetyan <108987460+Artyom7577@users.noreply.github.com>
2023-02-01 19:28:59 +0400

    Add files via upload

---
+++ BitVector.java
@@ -0,0 +1,56 @@
+import java.lang.Math;
+import java.util.ArrayList;
+public class BitVector {
+    private final ArrayList<Integer> ArrIndex = new ArrayList<>();
+    private int temp;
+    private final int size;
+    private final int [] arr;
+    BitVector(int size1){
+        this.size = size1;
+        int temp;
+        if (size1 % 32 == 0) {
+            temp = size1 / 32;
+        }else {
+            temp = (size1 / 32) + 1;
+        }
+        this.arr = new int[temp];
+    }
+    public void set(int index) {
+        if(ArrIndex.contains(index)) {
+            throw new IllegalArgumentException("Cant Set two same Elements");
+        }
+        ArrIndex.add(index);
+        if (index == 0 || index > size) {
+            throw new IllegalArgumentException("haven't 0 index or index > size");
+        }
+        if(index == 1) {
+            arr[temp] = arr[0] ^ 1;
+//            System.out.println(arr[temp]);
+            return;
+        }
+        this.temp = index / 32;
+        arr[temp] = arr[temp] ^ ( int )Math.pow(2, (index - 1) % 32 );
+//        System.out.println(arr[temp]);
+    }
+//    public void showArr(){
+//        for (int j : arr) {
+//            System.out.print(j + " ");
+//        }
+//        System.out.println();
+//        System.out.println(ArrIndex);
+//    }
+    public void reset(int index){
+        if(!ArrIndex.contains(index)) {
+            throw new IllegalArgumentException("Cant Reset two same Elements");
+        }
+        for (int i = 0; i < ArrIndex.size(); i++) {
+            if (ArrIndex.get(i) == index) {
+                ArrIndex.remove(i);
+            }
+        }
+        if(index == 1) {
+            arr[temp] = arr[temp] ^ 1;
+        }
+        arr[temp] = arr[temp] - ( int )Math.pow(2, (index - 1) % 32 );
+    }
+}
